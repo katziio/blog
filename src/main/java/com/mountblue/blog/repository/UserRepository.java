1fa72131347d93597a4c84c4a09514062220c129
Vignesh <vignesh000129@gmail.com>
2023-10-31 23:42:33 +0530

    auth files added

--- src/main/java/com/mountblue/blog/repository/UserRepository.java
+++ src/main/java/com/mountblue/blog/repository/UserRepository.java
@@ -9,11 +9,14 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email")
     Optional<User> findByUserEmail(@Param("email") String email);
 
     @Query("SELECT u FROM User u WHERE u.email = :email AND u.password = :password")
     Optional<User> findByEmailPassword(String email, String password);
+
+    @Query("SELECT u FROM User u WHERE u.name = :username")
+    Optional<User> findByUsername(@Param("username") String username);
 }
---
+++ src/main/java/com/mountblue/blog/service/auth/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.mountblue.blog.service.auth;
+
+import com.mountblue.blog.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user){
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(user.getRole().toString());
+        System.out.println(user.getRole());
+        return List.of(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
